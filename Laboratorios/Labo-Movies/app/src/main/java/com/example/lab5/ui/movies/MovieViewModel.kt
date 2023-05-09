a57f8f746299621a9fabd08e884d60912122bb82
Carmen Santacruz <00509191@uca.edu.sv>
2023-05-09 11:59:42 -0600

    Labo 7

--- Laboratorios/Labo-Movies/app/src/main/java/com/example/lab5/ui/movies/MovieViewModel.kt
+++ Laboratorios/Labo-Movies/app/src/main/java/com/example/lab5/ui/movies/MovieViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.lab5.ui.movies
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
@@ -10,9 +11,55 @@ import com.example.lab5.repositories.MovieRepository
 
 class MovieViewModel(private  val repository: MovieRepository) : ViewModel() {
 
+    var name = MutableLiveData("")
+    var category = MutableLiveData("")
+    var description = MutableLiveData("")
+    var calification = MutableLiveData("")
+    var status = MutableLiveData("")
+
     fun getMovies() = repository.getMovies()
 
     fun addMovies(movie: MovieModel) = repository.addMovies(movie)
+
+    fun createMovie(){
+
+        if (!validateData()) {
+            status.value = WRONG_INFORMATION
+            return
+        }
+
+        val movie = MovieModel(
+            name.value!!,
+            category.value!!,
+            description.value!!,
+            calification.value!!
+        )
+
+        addMovies(movie)
+        clearData()
+        status.value = MOVIE_CREATED
+    }
+
+    private fun validateData(): Boolean{
+        when{
+            name.value.isNullOrEmpty() -> return false
+            category.value.isNullOrEmpty() -> return false
+            description.value.isNullOrEmpty() -> return false
+            calification.value.isNullOrEmpty() -> return false
+        }
+        return true
+    }
+
+    private fun clearData(){
+        name.value=""
+        description.value=""
+        category.value=""
+        calification.value=""
+    }
+
+    fun clearStatus(){
+        status.value= INACTIVE
+    }
 
     companion object {
         val Factory = viewModelFactory {
@@ -21,5 +68,8 @@ class MovieViewModel(private  val repository: MovieRepository) : ViewModel() {
                 MovieViewModel(app.movieRepository)
             }
         }
+        const val MOVIE_CREATED = "Movie Created"
+        const val WRONG_INFORMATION = "Wrong Information"
+        const val INACTIVE = ""
     }
 }
--- Laboratorios/Labo-Movies/app/src/main/java/com/example/lab5/ui/movies/Movie_new.kt
+++ Laboratorios/Labo-Movies/app/src/main/java/com/example/lab5/ui/movies/Movie_new.kt
@@ -1,6 +1,7 @@
 package com.example.lab5.ui.movies
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.lab5.R
 import com.example.lab5.data.model.MovieModel
+import com.example.lab5.databinding.FragmentMovieNewBinding
 
 class Movie_new : Fragment() {
 
@@ -24,29 +26,57 @@ class Movie_new : Fragment() {
     private lateinit var calificationEditText: EditText
     private lateinit var submitButton: Button
 
+    private lateinit var binding: FragmentMovieNewBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_new, container, false)
+        //return inflater.inflate(R.layout.fragment_movie_new, container, false)
+        binding= FragmentMovieNewBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    private fun setViewModel(){
+        binding.viewmodel=movieViewModel
+    }
+
+    private fun observeStatus() {
+        movieViewModel.status.observe(viewLifecycleOwner) { status ->
+            when{
+                status.equals(MovieViewModel.WRONG_INFORMATION) -> {
+                    Log.d(APP_TAG, status)
+                    movieViewModel.clearStatus()
+                }
+                status.equals(MovieViewModel.MOVIE_CREATED) -> {
+                    Log.d(APP_TAG, status)
+                    Log.d(APP_TAG, movieViewModel.getMovies().toString())
+
+                    movieViewModel.clearStatus()
+                    findNavController().popBackStack()
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bind()
+        /*bind()
         submitButton.setOnClickListener{
             addMovie()
-        }
+        }*/
+        setViewModel()
+        observeStatus()
     }
 
-    fun bind(){
+    /*fun bind(){
         nameEditText = view?.findViewById(R.id.nameFieldData) !!
         categoryEditText = view?.findViewById(R.id.categoryFieldData) !!
         descriptionEditText = view?.findViewById(R.id.descriptionFieldData) !!
         calificationEditText = view?.findViewById(R.id.calificationFieldData) !!
         submitButton = view?.findViewById(R.id.newmovieButton) !!
-    }
+    }*/
 
     private fun addMovie(){
         val name = nameEditText.text.toString()
@@ -59,5 +89,9 @@ class Movie_new : Fragment() {
         movieViewModel.addMovies(movie)
 
         findNavController().popBackStack()
+    }
+
+    companion object{
+        const val APP_TAG = "APP TAG"
     }
 }
